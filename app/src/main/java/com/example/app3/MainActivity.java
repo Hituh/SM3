5d2bd8941dc448ee66a9f5145b8f20b12c13ed2a
Hituh <okazysssie@gmail.com>
2022-10-23 19:44:36 +0200

    FInal

--- app/src/main/java/com/example/app3/MainActivity.java
+++ app/src/main/java/com/example/app3/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.app3;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected Fragment createFragment() {
+        UUID taskId = (UUID) getIntent().getSerializableExtra(TaskListFragment.KEY_EXTRA_TASK_ID);
+        return TaskFragment.newInstance(taskId);
     }
+
 }
